addc9744cce6f70108248d090e19df36f496a62d
Niklas Minth <niklas.minth@gmail.com>
2024-06-29 12:46:49 +0200

    feat: Implemented search input elements

--- frontend/src/app/overview/overview-page/overview-page.component.ts
+++ frontend/src/app/overview/overview-page/overview-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ProposalType } from '../../proposal/interfaces/proposal-type';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-overview-page',
   standalone: true,
   templateUrl: './overview-page.component.html',
   styleUrl: './overview-page.component.scss',
-  imports: [],
+  imports: [SharedModule],
 })
-export class OverviewPageComponent {}
+export class OverviewPageComponent {
+  currentSearchValue: string | undefined;
+  antraege = new FormControl('');
+  selectedType = ProposalType.STR_ANTRAG;
+
+  antragsOptions: string[] = Object.values(ProposalType);
+
+  public changeSelectedType(type: ProposalType) {
+    this.selectedType = type;
+  }
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,11 +4,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
-
 @NgModule({
   declarations: [],
   imports: [
@@ -21,6 +26,12 @@ import { ToastrModule } from 'ngx-toastr';
     MatPaginatorModule,
     RouterLink,
     RouterLinkActive,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
   ],
   exports: [
     CommonModule,
@@ -30,6 +41,12 @@ import { ToastrModule } from 'ngx-toastr';
     MatPaginatorModule,
     RouterLink,
     RouterLinkActive,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'ja-JP' },
