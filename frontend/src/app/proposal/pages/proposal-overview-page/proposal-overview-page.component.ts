eaf4d3d461d2d1b46dc470b487f2e0f9ca7e9a24
Niklas Minth <niklas.minth@gmail.com>
2024-07-02 13:17:05 +0200

    chore: Docker setup

--- frontend/src/app/proposal/pages/proposal-overview-page/proposal-overview-page.component.ts
+++ frontend/src/app/proposal/pages/proposal-overview-page/proposal-overview-page.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { FrameComponent } from '../../../shared/components/frame/frame.component';
+import { FilterService } from '../../../shared/services/filter.service';
 import { ProposalOverviewFilterComponent } from '../../components/proposal-overview-filter/proposal-overview-filter.component';
 import { ProposalOverviewTableComponent } from '../../components/proposal-overview-table/proposal-overview-table.component';
 import { IProposal } from '../../interfaces/proposal';
@@ -23,10 +24,14 @@ import { ProposalState } from '../../states/proposal-overview.state';
 })
 export class ProposalOverviewPageComponent implements OnInit {
   @Select(ProposalState.proposals) proposals$!: Observable<IProposal>;
-  public activeFilter = false;
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private filterService: FilterService
+  ) {}
   ngOnInit(): void {
-    this.store.dispatch(new getProposals());
+    if (!this.filterService.filterState) {
+      this.store.dispatch(new getProposals());
+    }
   }
 }
