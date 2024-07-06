4737cfa93305310ef9bec60edf29662cbb0e258d
Niklas Minth <niklas.minth@gmail.com>
2024-07-06 11:00:30 +0200

    chore: FE API-URL remove slash

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl:
-    'https://ukmunzfmaqksbiph543vtyhge40uhubv.lambda-url.eu-north-1.on.aws/',
+    'https://ukmunzfmaqksbiph543vtyhge40uhubv.lambda-url.eu-north-1.on.aws',
 };
